3c2ee7b7b3e1ed4b8e38d966dc5c6fe01db35efd
Khethelo Nkosi <saneleknkosi@gmail.com>
2025-05-08 11:14:35 +0200

    functionality(code): Added code to read from pseudo database

--- LeaderBoard/src/main/java/khethelo/Player.java
+++ LeaderBoard/src/main/java/khethelo/Player.java
@@ -1,6 +1,12 @@
 package khethelo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
+
+import org.json.JSONObject;
 
 public class Player {
 
@@ -10,6 +16,8 @@ public class Player {
 
     public static HashMap<String, String> players = new HashMap<>();
 
+    private static final String DATABASE_FILE = "players.json";
+
     Player() {
     }
 
@@ -18,7 +26,7 @@ public class Player {
         this.playerPassword = playerPassword;
         this.playerScore = 0;
 
-        players.put(playerName, playerPassword);
+        savePlayerToDatabase(playerName, playerPassword);
     }
 
     public String getName() {
@@ -30,11 +38,13 @@ public class Player {
     }
 
     public boolean playerExists(String playerName) {
-        return players.containsKey(playerName);
+        JSONObject database = loadDatabase();
+        return database.has(playerName);
     }
 
     public boolean validatePassword(String username, String password) {
-        return players.containsKey(username) && players.get(username).equals(password);
+        JSONObject database = loadDatabase();
+        return database.has(username) && database.getString(username).equals(password);
     }
 
 
@@ -42,4 +52,26 @@ public class Player {
     //     String savedPass =  playerExists(username) ? players.get(username) : null;
     //     return false;
     // }
+
+    public void savePlayerToDatabase(String playerName, String playerPassword) {
+        JSONObject database = loadDatabase();
+        database.put(playerName, playerPassword);
+        try {
+            Files.write(Paths.get(DATABASE_FILE), database.toString(4).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            System.err.println("Error saving to database: " + e.getMessage());
+        }
+    }
+
+    private JSONObject loadDatabase() {
+        try {
+            if (Files.exists(Paths.get(DATABASE_FILE))) {
+                String content = new String(Files.readAllBytes(Paths.get(DATABASE_FILE)));
+                return new JSONObject(content);
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading database: " + e.getMessage());
+        }
+        return new JSONObject(); // Return an empty database if file doesn't exist or an error occurs
+    }
 }
